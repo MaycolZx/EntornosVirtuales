0b70f997c046b287971b4142042d64fba75c0aeb
Maycol-Zx-PC <alexander51600@hotmail.com>
2022-10-03 21:17:15 -0500

    :tada: Init project

--- entorno01/app/main.py
+++ entorno01/app/main.py
@@ -9,7 +9,11 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@app.route('send')
+def handle_message(data):
+    selection = data["vote"]
+    emit("announce",{"selection":selection},broadcast = True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
     socketio.run(app)
--- entorno01/app/static/index.js
+++ entorno01/app/static/index.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 var socket = io();
     socket.on('connect', function() {
-        // document.querySelectorAll("button");
-        socket.emit('my event', {data: 'I\'m connected!'});
+        document.querySelectorAll("button").forEach(button =>{
+            button.onclick = () => {
+                const vote = button.dataset.vote;
+                socket.emit('send',{"vote":vote})
+            };
+        })
+        // button = document.querySelectorAll("button")[1]
+        // button.dateset.vote
+        // button = document.querySelectorAll("button")[2]
+        // button.dateset.vote
+        // socket.emit('my event', {data: 'I\'m connected!'});
     });
 });
 
